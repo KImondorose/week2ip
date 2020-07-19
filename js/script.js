702cee3fb170a16778fac248eb4d1c35d8a635da
Kimondorose <rosewairimu1@gmail.com>
2020-07-19 22:25:22 +0300

    add js prompts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let date = function (dateOfBirth){
+     prompt (dateOfBirth);
+    };
+    date ("Enter Your Date of Birth"); 
+
+    let month = function (monthOfBirth){
+         prompt (monthOfBirth);
+       };
+       month ("Enter Your Month of Birth");
+
+    let year = function (yearOfBirth){
+        prompt (yearOfBirth);
+       };
+       year ("Enter Your Year of Birth");
+    
+    let yourGender = function (gender){
+       prompt  (gender);
+       };
+       yourGender ("Enter Your Gender"); 
+    
