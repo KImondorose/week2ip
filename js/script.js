1592377a242e2131955f05616630afd9d8cee1ee
Kimondorose <rosewairimu1@gmail.com>
2020-07-19 22:41:15 +0300

    try to add javascript functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
-let date = function (dateOfBirth){
+let DD = function (dateOfBirth){
      prompt (dateOfBirth);
     };
-    date ("Enter Your Date of Birth"); 
+    DD ("Enter Your Date of Birth as two numbers"); 
 
-    let month = function (monthOfBirth){
+DD = TRUE;
+if (DD >31) {
+    console.log ("Continue");
+}
+else if (DD<1) {
+    console.log ("Continue");
+}
+else {
+    console.log ("Please enter a valid date");
+}
+
+
+
+let MM = function (monthOfBirth){
          prompt (monthOfBirth);
        };
-       month ("Enter Your Month of Birth");
+       MM ("Enter Your Month of Birth as two numbers");
 
-    let year = function (yearOfBirth){
+let CC = function (yearOfBirth){
         prompt (yearOfBirth);
        };
-       year ("Enter Your Year of Birth");
+       CC ("Enter Your Year of Birth as first two numbers");
+
+let YY = function (yearOfBirth){
+        prompt (yearOfBirth);
+       };
+       YY ("Enter Your Year of Birth as last two numbers");
     
-    let yourGender = function (gender){
+
+    
+let yourGender = function (gender){
        prompt  (gender);
        };
-       yourGender ("Enter Your Gender"); 
-    
+       yourGender ("Enter Your Gender as F or M"); 
+
+
