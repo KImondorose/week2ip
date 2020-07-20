8cde99e865472881972cee8e61353942266559c3
Kimondorose <rosewairimu1@gmail.com>
2020-07-20 09:22:45 +0300

    add functions for give gender and give century

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ function giveName() {
     return false;
   }
 }
+function selectGender() {
+    var gender = document.getElementById("gender").value;
+    if (gender == "1") {
+      alert("Gender must be selected");
+      return false;
+    }
+  }
+  
+  function giveCentury() {
+    var century = document.getElementById("centuryBorn").value;
+    if (century == "") {
+      alert("Century must be filled out");
+      return false;
+    }
+  }
